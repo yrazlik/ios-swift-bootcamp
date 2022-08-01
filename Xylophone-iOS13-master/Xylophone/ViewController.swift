7a61f554b1f112e71e43ad025823a5f32d526510
Yasin Razlık <yrazlik@gmail.com>
2022-08-01 21:18:11 +0300

    change button opacity when pressed challenge completed

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
         playSound(button : (sender.titleLabel?.text)!)
     }
     
